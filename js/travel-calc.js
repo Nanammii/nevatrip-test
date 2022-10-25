a61c34639e30599f87d5daa58080e44103c30134
Nanammii <kaddia@gmail.com>
2022-10-25 22:55:37 +0700

    fixed js

--- js/travel-calc.js
+++ js/travel-calc.js
@@ -19,13 +19,23 @@ const time = document.getElementById('time');
 const tiketNum = document.getElementById('num');
 
 buttonSum.addEventListener('click', function (e) {
-  let price = routeMain * tiketNum.value;
-  console.log(price);
-  const div = document.createElement('p');
-  div.classList.add('main-container__wrapp--text');
-  div.innerHTML = `<p class="main-container__text">Вы выбрали ${tiketNum.value} билета по маршруту из A в B стоимостью ${price} руб.</p>
+  if (route.value === 'из A в B и обратно в А') {
+    let price = routeABA * tiketNum.value;
+    const div = document.createElement('p');
+    div.classList.add('main-container__wrapp--text');
+    div.innerHTML = `<p class="main-container__text">Вы выбрали ${tiketNum.value} билета по маршруту из A в B стоимостью ${price} руб.</p>
 <p class="main-container__text">Это путешествие займет у вас ${timeTravel} минут.</p>
 <p class="main-container__text">Теплоход отправляется в ${time.value}, а прибудет в 18-00.</p>`;
-  form.appendChild(div);
+    form.appendChild(div);
+  } else {
+    let price = routeMain * tiketNum.value;
+    console.log(price);
+    const div = document.createElement('p');
+    div.classList.add('main-container__wrapp--text');
+    div.innerHTML = `<p class="main-container__text">Вы выбрали ${tiketNum.value} билета по маршруту из A в B стоимостью ${price} руб.</p>
+<p class="main-container__text">Это путешествие займет у вас ${timeTravel} минут.</p>
+<p class="main-container__text">Теплоход отправляется в ${time.value}, а прибудет в 18-00.</p>`;
+    form.appendChild(div);
+  }
 })
 
